6e6fdfb5e46a80e68a7962f43d1d255c6edec080
zhyunk <gimwlgus@gmail.com>
2024-01-13 09:13:14 +0900

    feat : filter에서 경로 변경

--- gateway/src/main/java/kim/zhyun/gateway/filter/CustomFilter.java
+++ gateway/src/main/java/kim/zhyun/gateway/filter/CustomFilter.java
@@ -3,13 +3,15 @@ package kim.zhyun.gateway.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.http.HttpMethod.GET;
 
 @Slf4j
 @Component
@@ -22,32 +24,38 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            String uri = request.getURI().toString();
+            ServerHttpRequest req = exchange.getRequest();
+            String path = req.getPath().toString();
             
-            log.info("💁 %s".formatted(uri));
+            log.info("💁 %s".formatted(path));
             
-            String method = request.getMethod().name();
+            String method = req.getMethod().name();
             switch (method) {
                 case  "GET", "DELETE"   -> log.info("💁 Is the method GET or DELETE --> %s".formatted(method));
                 case  "PUT", "PATCH"    -> log.info("💁 update 🔨🔧 --> %s".formatted(method));
                 case  "POST"            -> log.info("💁 new save 💾 --> %s".formatted(method));
             }
             
-            if (uri.contains("/error") || uri.contains("/exception"))
-                return exception(exchange, uri, "my exception");
+            if (path.contains("/error") || path.contains("/exception")) {
+                req = redirect(exchange, path, "move to /rewrite-path");
+            }
             
-            return chain.filter(exchange);
+            return chain.filter(exchange.mutate().request(req).build());
         };
     }
 
     
-    private static Mono<Void> exception(ServerWebExchange exchange, String uri, String message) {
+    private static ServerHttpRequest redirect(ServerWebExchange exchange, String path, String message) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(HttpStatus.I_AM_A_TEAPOT);
         
-        log.info("💁 exception !! --> %s\n%s".formatted(uri, message));
-        return response.setComplete();
+        log.info("💁 exception !! --> %s\n%s".formatted(path, message));
+        
+        URI transformedUri = exchange.getRequest().getURI().resolve("/%s/rewrite-path/in".formatted(path.split("/")[1]));
+
+        return exchange.getRequest().mutate()
+                .method(GET)
+                .uri(transformedUri).build();
     }
     
     public static class Config { }
